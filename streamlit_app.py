e190f12fdf6c0e6a949f2eca2501b18d498eba59
sheryK <42703812+sheryK@users.noreply.github.com>
2024-02-07 11:58:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🦜🔗 Quickstart App')
 
 st.write('Hello world!')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'Enter your use case here and ask for test cases.')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠️')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
